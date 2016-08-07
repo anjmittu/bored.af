fd27ce6db7e76808a47b65c0d22cebff47f67770
Ubuntu <ubuntu@ip-172-31-63-88.ec2.internal>
2016-08-07 07:24:55 +0000

    h

--- getsimilarusers.php
+++ getsimilarusers.php
@@ -4,7 +4,7 @@ $servername = "";
 $username = "";
 $password = "";
 $dbname = "";
-$return = ();
+$return = array();
 
 try{
 	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
@@ -19,6 +19,7 @@ try{
 		$result=$result->fetchAll();
 		$user_id = $result[0]["user_id"];
 
+		$returnjs = '{"users": [';
 		$sql = "SELECT name FROM interest WHERE user_id = '$user_id';";
 		$result = $conn->query($sql);
 
@@ -28,20 +29,21 @@ try{
 		   	   $interest = $row['name'];
 			   $sql = "SELECT user_id FROM interest WHERE name = '$interest';";
 			   $result2 = $conn->query($sql);
-			   $result2=$result2->fetchAll();
-			   foreach ($result2 as $row2) {
-			   	   array_merge($return,array($row2['user_id']);
+			   if($result2->rowCount() >= 1){
+			   	foreach ($result2 as $row2) {
+					$returnjs = $returnjs.'"'.$row2['user_id'].'"'.', ';
+			   	}
+			  
+			   } else {
+				fail();
 			   }
 		   }
 
-		   $returnjs = '{"users": [';
-		   foreach ($return as $user) {
-		   	$returnjs=$returnjs.'"'.$user.'"'.', ';
-		   }
-		   $returnjs=substr($returnjs, 0, (strlen($returnjs)-2));
-		   $returnjs=$returnjs.']}';
+		   $returnjs = substr($returnjs, 0, (strlen($returnjs)-2));
+		   $returnjs = $returnjs.']}';
 
 		   echo $returnjs;
+		   
 		 } else {
 		  fail();
 		}
@@ -54,7 +56,7 @@ try{
 }
 
 function fail(){
-	echo '{"interests": []}';
+	echo '{"users": []}';
 }
 
 ?>

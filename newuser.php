d6379c7e14f83d6c4025d1f6bc62d6fae7f9fd29
Nick Mittu <nick@mittudev.com>
2016-08-06 21:28:49 -0400

    Fix newuser.php

--- newuser.php
+++ newuser.php
@@ -9,12 +9,14 @@ try{
 	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	$sql = "INSERT INTO user VALUE(\"{$_POST["fname"]}\",
-							\"{$_POST["lname"]}\",
-							\"{$_POST["email"]}\",
-							\"{password_hash($_POST["fname"])}\",
-							\"{$_POST["bday"]}\",
-							'{$_POST["gender"]}',);";
+	$fname = mysql_real_escape_string($_POST["fname"]);
+	$lname = mysql_real_escape_string($_POST["lname"]);
+	$email = mysql_real_escape_string($_POST["email"]);
+	$pass = mysql_real_escape_string(password_hash($_POST["pass"]));
+	$bday = mysql_real_escape_string($_POST["bday"]);
+	$gender = mysql_real_escape_string($_POST["gender"]);
+
+	$sql = "INSERT INTO user VALUE('$fname', '$lname', '$pass', '$email', '$bday', '$gender');";
 
 	$conn->exec($sql);
 	echo '{"completed": true}';

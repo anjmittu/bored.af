e681ebde78b04a275707acf3d60a84b04a249645
Nick Mittu <nick@mittudev.com>
2016-08-06 22:26:20 -0400

    Fix Login.php

--- login.php
+++ login.php
@@ -9,7 +9,9 @@ try{
 	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	$sql = "SELECT email, pass_hash FROM user WHERE email = ".$_POST["email"].";";
+    $email = $_POST['email'];
+
+	$sql = "SELECT email, pass_hash FROM user WHERE email = $email;";
 	$result = $conn->query($sql);
 	if(!count($result) == 1){
 		if($result[1]["email"] ==  $_POST["email"]){

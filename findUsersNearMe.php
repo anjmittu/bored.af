73773a62b02bc390611db7c1329f6ec9ee425338
Ubuntu <ubuntu@ip-172-31-63-88.ec2.internal>
2016-08-07 12:16:08 +0000

    fix location

--- findUsersNearMe.php
+++ findUsersNearMe.php
@@ -1,30 +1,9 @@
 <?php
-<?php
 
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
-
-
-$url = '52.206.229.115/login.php';
-$data = array('email' => $_POST["email"], 'password' => $_POST["pass"]);
-
-// use key 'http' even if you send the request to https://...
-$options = array(
-    'http' => array(
-        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method'  => 'POST',
-        'content' => http_build_query($data)
-    )
-);
-$context  = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
-if ($result === FALSE) { fail(); die();}
-
-if(!json_decode($result)["verified"]){
-    fail(); die;
-}
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
 
 try{
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
@@ -32,36 +11,42 @@ try{
 
     $email = $_POST['email'];
 
-    $sql = "SELECT id, lon, lat FROM user JOIN location USING(user_id) WHERE email = $email;";
-    $result = $conn->query($sql)->fetchAll();
+    $sql = "SELECT user_id, lon, lat FROM user JOIN location USING(user_id) WHERE email = '$email';";
+    $result = $conn->query($sql);
 
+    if($result->rowCount()!=1){
+	fail();
+	die();
+    }
 
-    $id = $result[0]["id"];
-    $lon = $result[0]["lon"];
-    $lat = $result[0]["lat"];
+    $result = $result->fetchAll();
 
-    $sql = "SELECT user_id, email, lon AS lon2, lat AS lat2, RADIANS(lat2-$lat) AS dLat, RADIANS(lon2-$lon) AS dLon, RADIANS($lat) AS rLat1,
-            RADIANS(lat2) AS rLat2, SIN(dLat/2)*SIN(dLat/2) + SIN(dLon/2) * SIN(dLon/2) * COS(rLat1) * COS(rLat2) AS a, 2*ATAN2(SQRT(a),
-            SQRT(1-a)) AS c, 6371*c AS d FROM location JOIN user USING(user_id) WHERE d<1;";
+    $id = $result[0]["user_id"];
+    $lon_ = $result[0]["lon"];
+    $lat_ = $result[0]["lat"];
+
+    $sql = "SELECT user_id, email, lon, lat, ABS($lat_ - lat) AS dlat, ABS($lon_ - lon) AS dlon FROM location JOIN user USING(user_id) WHERE SQRT((ABS($lat_ - lat)*ABS($lat_-lat)) +  (ABS($lon_-lon)*ABS($lon_-lon)))<(1/111);" ;
+    
+
     $result = $conn->query($sql);
 
     $return = '{"users": [';
     foreach ($result as $row){
         $return .= '"'.$row["email"].'"'.", ";
     }
-    $result = substr($return, 0, strlen($return)-2).']}';
+    $return = substr($return, 0, strlen($return)-2).']}';
 
     echo $return;
 
 
 
 }catch(PDOException $e) {
-    fail();
+    	echo $e->getMessage();
+	fail();
 }
 
 function fail(){
     echo '{"users": []}';
 }
 
-?>
 ?>
